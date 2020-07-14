51797e4966afb7607cdff6b88c5be317956cf776
Joe Bay <jbay55278@gmail.com>
2020-07-14 13:32:51 -0800

    finished comment, and comments, finishing the project without stretch

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -5,13 +5,14 @@ import './Comments.css';
 const Comments = props => {
   // Make sure the parent of Comments is passing the right props!
   const { comments } = props;
+  console.log(comments[0])
 
   return (
     <div>
       {/* map through the comments data array and render a Comment for every comment piece of data */}
       {
         comments.map(item => {
-          return <Comment comment={comments}/>
+          return <Comment comment={item}/>
         })
       }
     </div>
